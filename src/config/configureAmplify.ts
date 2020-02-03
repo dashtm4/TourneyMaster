c226595e63f369146b18937cc57389ef3e6beec2
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2020-02-03 17:40:38 +0200

    TM-92: Don't read the config from local file, move variables to the ENV variables

--- src/config/configureAmplify.ts
+++ src/config/configureAmplify.ts
@@ -1,20 +1,14 @@
 import Amplify from 'aws-amplify';
-import config from './config.json';
 
 export default () => {
-  const isProd = process.env.NODE_ENV !== 'production';
   Amplify.configure({
     Auth: {
       mandatorySignId: true,
-      region: isProd ? process.env.AWS_REGION : config.cognito.REGION,
-      userPoolId: isProd
-        ? process.env.AWS_USER_POOL_ID
-        : config.cognito.USER_POOL_ID,
-      userPoolWebClientId: isProd
-        ? process.env.AWS_USER_POOL_WEB_CLIENT_ID
-        : config.cognito.APP_CLIENT_ID,
+      region: process.env.REACT_APP_AWS_REGION,
+      userPoolId: process.env.REACT_APP_AWS_USER_POOL_ID,
+      userPoolWebClientId: process.env.REACT_APP_AWS_USER_POOL_WEB_CLIENT_ID,
       oauth: {
-        domain: isProd ? process.env.AWS_COGNITO_DOMAIN : config.cognito.DOMAIN,
+        domain: process.env.REACT_APP_AWS_COGNITO_DOMAIN,
         redirectSignIn: 'http://localhost:3000/',
         redirectSignOut: 'http://localhost:3000/',
         responseType: 'token',
