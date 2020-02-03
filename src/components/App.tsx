1283debff19d08d612123299b536c794192f5433
what1s1ove <zubko.2007@gmail.com>
2020-02-03 11:28:38 +0200

    TM-86: StorybookComponents + dashboard-menu

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import LoginPage from './login-page';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import LoginPage from './login-page';
 
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" component={LoginPage} />
-      </Switch>
-    </Router>
-  );
-};
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route path="/" component={LoginPage} />
+//       </Switch>
+//     </Router>
+//   );
+// };
+
+import Menu from '../components/common/dashboard-menu';
+
+const App = () => <Menu />;
 
 export default App;
