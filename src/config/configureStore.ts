8a50150b4d81af0ecee14da27e59e1ec78ebaba9
Yevhen <yevhen.prybytok@binary-studio.com>
2020-02-03 11:34:16 +0200

    Added redux, lodash-es, thunk middleware

---
+++ src/config/configureStore.ts
@@ -0,0 +1,16 @@
+import { createStore, PreloadedState, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from 'reducers/rootReducer';
+
+export default (preloadedState?: PreloadedState<any>) => {
+  const middlewares = [thunkMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+
+  return store;
+};
---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+import configureAmplify from './configureAmplify';
+import configureStore from './configureStore';
+
+export { configureAmplify, configureStore };
