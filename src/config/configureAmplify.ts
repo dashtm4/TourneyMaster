6417c50a89765dd935180a7823c4e48932bddce7
Oleksandr Kovalov <kovalov.oleksandr@binary-studio.com>
2020-02-03 15:05:32 +0200

    TM-92: read correct env variables in config

--- src/config/configureAmplify.ts
+++ src/config/configureAmplify.ts
@@ -2,14 +2,19 @@ import Amplify from 'aws-amplify';
 import config from './config.json';
 
 export default () => {
+  const isProd = process.env.NODE_ENV !== 'production';
   Amplify.configure({
     Auth: {
       mandatorySignId: true,
-      region: config.cognito.REGION,
-      userPoolId: config.cognito.USER_POOL_ID,
-      userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+      region: isProd ? process.env.AWS_REGION : config.cognito.REGION,
+      userPoolId: isProd
+        ? process.env.AWS_USER_POOL_ID
+        : config.cognito.USER_POOL_ID,
+      userPoolWebClientId: isProd
+        ? process.env.AWS_USER_POOL_WEB_CLIENT_ID
+        : config.cognito.APP_CLIENT_ID,
       oauth: {
-        domain: config.cognito.DOMAIN,
+        domain: isProd ? process.env.AWS_COGNITO_DOMAIN : config.cognito.DOMAIN,
         redirectSignIn: 'http://localhost:3000/',
         redirectSignOut: 'http://localhost:3000/',
         responseType: 'token',
