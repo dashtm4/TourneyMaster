9697aa16f4c862183a98335c9c1c240bf5a522cb
Yevhen <yevhen.prybytok@binary-studio.com>
2020-02-03 13:42:39 +0200

    Added rootReducer, initial userReducer

--- src/config/configureStore.ts
+++ src/config/configureStore.ts
@@ -1,9 +1,10 @@
-import { createStore, PreloadedState, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from 'reducers/rootReducer';
+import preloadedState from './preloadedState';
 
-export default (preloadedState?: PreloadedState<any>) => {
+export default () => {
   const middlewares = [thunkMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
