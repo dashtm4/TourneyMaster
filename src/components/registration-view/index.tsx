ad0b85afd6e3e43356094fe754ca083d8460ccb0
r-khanas <rostyslav.khanas@binary-studio.com>
2020-02-05 11:26:19 +0200

    TM-45 Registration View, add some more

--- src/components/registration-view/index.tsx
+++ src/components/registration-view/index.tsx
@@ -21,13 +21,103 @@ const LibraryManaget = () => (
       <li>
         <SectionDropdown>
           <span>Primary Information</span>
-          <p>Content</p>
+          <div className={styles['tournament-content']}>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Division:
+              </span>
+              <p>2020, 2012</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Open Date:
+              </span>
+              <p>01/01/20</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Close Date:
+              </span>
+              <p>01/31/20</p>
+
+              <span className={styles['tournament-status']} />
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Entry Fee:
+              </span>
+              <p>$100.00</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Deposit Fee:
+              </span>
+              <p>$25.00</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Early Bird Discount:
+              </span>
+              <p>None</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Discount End Date:
+              </span>
+              <p>-</p>
+            </div>
+          </div>
         </SectionDropdown>
       </li>
       <li>
         <SectionDropdown>
           <span>Teams & Athletes</span>
-          <p>Content</p>
+          <div className={styles['tournament-content']}>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Max Teams Per Division:
+              </span>
+              <p>-</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Min Athletes on Roster:
+              </span>
+              <p>-</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Max Athletes on Roster:
+              </span>
+              <p>-</p>
+
+              <span className={styles['tournament-status']} />
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Entry Fee:
+              </span>
+              <p>$100.00</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Deposit Fee:
+              </span>
+              <p>$25.00</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Early Bird Discount:
+              </span>
+              <p>None</p>
+            </div>
+            <div className={styles['tournament-content-item']}>
+              <span className={styles['tournament-content-title']}>
+                Discount End Date:
+              </span>
+              <p>-</p>
+            </div>
+          </div>
         </SectionDropdown>
       </li>
       <li>
