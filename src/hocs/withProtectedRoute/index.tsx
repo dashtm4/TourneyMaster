a58ddc2578803d51b8d4eae5cdd83e88ea4ea617
what1s1ove <zubko.2007@gmail.com>
2020-02-10 09:02:40 +0200

    TM-106-RegisterNow: * word mistakes

--- src/hocs/withProtectedRoute/index.tsx
+++ src/hocs/withProtectedRoute/index.tsx
@@ -5,9 +5,9 @@ import { Routes } from '../../common/constants';
 const withProtectedRoute = (Component: React.ComponentType<any>) => (
   props: any
 ) => {
-  const userToket = localStorage.getItem('token');
+  const userToken = localStorage.getItem('token');
 
-  return Boolean(userToket) ? (
+  return Boolean(userToken) ? (
     <Component {...props} />
   ) : (
     <Redirect to={Routes.LOGIN} />
--- src/hocs/withUnprotectedRoute/index.tsx
+++ src/hocs/withUnprotectedRoute/index.tsx
@@ -5,9 +5,9 @@ import { Routes } from '../../common/constants';
 const withUnprotectedRoute = (Component: React.ComponentType<any>) => (
   props: any
 ) => {
-  const userToket = localStorage.getItem('token');
+  const userToken = localStorage.getItem('token');
 
-  return Boolean(userToket) ? (
+  return Boolean(userToken) ? (
     <Redirect to={Routes.DEFAULT} />
   ) : (
     <Component {...props} />
