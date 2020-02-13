5de83a5f3c60b8da2b9e77ff4e4d4ce47dd1dd26
what1s1ove <zubko.2007@gmail.com>
2020-02-13 09:40:40 +0200

    TM-75-ScoringBig: + styles for modal-wrapper

--- src/components/common/modal/index.tsx
+++ src/components/common/modal/index.tsx
@@ -11,29 +11,40 @@ interface Props {
   children: React.ReactElement;
 }
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles(
   createStyles({
     modal: {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    modalWrapper: {
+      padding: '25px 35px',
+      backgroundColor: '#F4F4F4',
+      borderRadius: '5px',
+      boxShadow: 'box-shadow: 0 1px 10px 0 rgba(0,0,0,0.1)',
+    },
   })
 );
 
-const ModalRoot = ({ isOpen, onClose, children }: Props) => (
-  <Modal
-    className={useStyles().modal}
-    onClose={onClose}
-    open={isOpen}
-    closeAfterTransition
-    BackdropComponent={Backdrop}
-    BackdropProps={{ timeout: 500 }}
-    aria-labelledby="transition-modal-title"
-    aria-describedby="transition-modal-description"
-  >
-    <Fade in={isOpen}>{children}</Fade>
-  </Modal>
-);
+const ModalRoot = ({ isOpen, onClose, children }: Props) => {
+  const styles = useStyles();
 
+  return (
+    <Modal
+      className={styles.modal}
+      onClose={onClose}
+      open={isOpen}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{ timeout: 500 }}
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+    >
+      <Fade in={isOpen}>
+        <div className={styles.modalWrapper}>{children}</div>
+      </Fade>
+    </Modal>
+  );
+};
 export default ModalRoot;
