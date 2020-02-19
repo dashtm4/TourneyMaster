e11468fa89a17ab4d4d9e8d0bd73a139bec93421
what1s1ove <zubko.2007@gmail.com>
2020-02-19 09:54:13 +0200

    TM-106-RegisterNov: + simple user-manu

--- src/components/header/user-info/index.tsx
+++ src/components/header/user-info/index.tsx
@@ -1,20 +1,55 @@
 import React from 'react';
 import { Auth } from 'aws-amplify';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import styles from './style.module.scss';
 
+const USER_LOGO = '';
+
 const UserInfo: React.FC = () => {
-  const userLogo = '';
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <div
-      onClick={() => {
-        Auth.signOut();
+    <ul className={styles.userList}>
+      <li>
+        <p className={styles.userImgWrapper}>
+          <img src={USER_LOGO} width="36" height="36" alt="" />
+          <button
+            className={styles.userNavBtn}
+            onClick={handleClick}
+            aria-controls="user-nav-menu"
+            aria-haspopup="true"
+          >
+            <span className="visually-hidden">Open menu</span>
+          </button>
+        </p>
+        <Menu
+          id="user-nav-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem
+            onClick={() => {
+              Auth.signOut();
 
-        localStorage.removeItem('token');
-      }}
-      style={{ backgroundImage: userLogo }}
-      className={styles.container}
-    />
+              localStorage.removeItem('token');
+            }}
+          >
+            Sign out
+          </MenuItem>
+        </Menu>
+      </li>
+    </ul>
   );
 };
 
