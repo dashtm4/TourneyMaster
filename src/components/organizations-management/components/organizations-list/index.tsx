667507219a0ebbbe8191c5ce6ffb77b96d963b48
what1s1ove <zubko.2007@gmail.com>
2020-03-03 18:03:33 +0200

    TM-146-Collaboration: + base delete-org popup

--- src/components/organizations-management/components/organizations-list/index.tsx
+++ src/components/organizations-management/components/organizations-list/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { SectionDropdown, HeadingLevelThree, Toasts } from 'components/common';
+import {
+  SectionDropdown,
+  HeadingLevelThree,
+  Toasts,
+  Button,
+} from 'components/common';
+import PopupDeleteOrganization from '../popup-delete-organization';
 import { IOrganization } from 'common/models';
 import { Icons } from 'common/constants';
 import { getIcon } from 'helpers';
@@ -25,69 +31,100 @@ const copyToClipboard = (id: string) => {
   Toasts.successToast('The invitation code was successfully copied');
 };
 
-const OrganizationsList = ({ organizations }: Props) => (
-  <SectionDropdown
-    type="section"
-    useBorder={true}
-    isDefaultExpanded={true}
-    panelDetailsType="flat"
-  >
-    <HeadingLevelThree>
-      <span>Organizations List</span>
-    </HeadingLevelThree>
-    <div className={styles.sectWrapper}>
-      {organizations.length > 0 ? (
-        <>
-          <p className={styles.description}>
-            Organizations to which you currently belong
-          </p>
-          <div className={styles.orgTableWrapper}>
-            <table className={styles.orgTable}>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Name</th>
-                  <th>@Tag</th>
-                  <th>City</th>
-                  <th>State</th>
-                  <th>Invitation Code</th>
-                  <th></th>
-                </tr>
-              </thead>
+const OrganizationsList = ({ organizations }: Props) => {
+  const [configOrg, onDeletePopup] = React.useState<null | IOrganization>(null);
 
-              <tbody>
-                {organizations
-                  .sort((a, b) => (a.org_name > b.org_name ? 1 : -1))
-                  .map((organization, index) => (
-                    <tr className={styles.listItem} key={organization.org_id}>
-                      <td>{`${index + 1}.`}</td>
-                      <td>{organization.org_name}</td>
-                      <td>{organization.org_tag}</td>
-                      <td>{organization.city}</td>
-                      <td>{organization.state}</td>
-                      <td>
-                        <button
-                          className={styles.codeBtn}
-                          onClick={() => copyToClipboard(organization?.org_id)}
-                        >
-                          {organization.org_id}
-                          {getIcon(Icons.FILE_COPY, COPY_ICON_STYLES)}
-                        </button>
-                      </td>
+  return (
+    <>
+      <SectionDropdown
+        type="section"
+        useBorder={true}
+        isDefaultExpanded={true}
+        panelDetailsType="flat"
+      >
+        <HeadingLevelThree>
+          <span>Organizations List</span>
+        </HeadingLevelThree>
+        <div className={styles.sectWrapper}>
+          {organizations.length > 0 ? (
+            <>
+              <p className={styles.description}>
+                Organizations to which you currently belong
+              </p>
+              <div className={styles.orgTableWrapper}>
+                <table className={styles.orgTable}>
+                  <thead>
+                    <tr>
+                      <th></th>
+                      <th>Name</th>
+                      <th>@Tag</th>
+                      <th>City</th>
+                      <th>State</th>
+                      <th>Invitation Code</th>
+                      <th></th>
                     </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>
-        </>
-      ) : (
-        <span className={styles.noFound}>
-          Sorry, you are not in organization yet. You can create your own or
-          apply invitation from other user.
-        </span>
+                  </thead>
+
+                  <tbody>
+                    {organizations
+                      .sort((a, b) => (a.org_name > b.org_name ? 1 : -1))
+                      .map((organization, index) => (
+                        <tr
+                          className={styles.listItem}
+                          key={organization.org_id}
+                        >
+                          <td>{`${index + 1}.`}</td>
+                          <td>{organization.org_name}</td>
+                          <td>{organization.org_tag}</td>
+                          <td>{organization.city}</td>
+                          <td>{organization.state}</td>
+                          <td>
+                            <button
+                              className={styles.codeBtn}
+                              onClick={() =>
+                                copyToClipboard(organization?.org_id)
+                              }
+                            >
+                              {organization.org_id}
+                              {getIcon(Icons.FILE_COPY, COPY_ICON_STYLES)}
+                            </button>
+                          </td>
+                          <td>
+                            <span className={styles.delBtnWrapper}>
+                              <Button
+                                onClick={() => onDeletePopup(organization)}
+                                icon={getIcon(Icons.DELETE)}
+                                label="Delete"
+                                variant="text"
+                                color="inherit"
+                              />
+                            </span>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+            </>
+          ) : (
+            <span className={styles.noFound}>
+              Sorry, you are not in organization yet. You can create your own or
+              apply invitation from other user.
+            </span>
+          )}
+        </div>
+      </SectionDropdown>
+      {configOrg && (
+        <PopupDeleteOrganization
+          deleteTitle={configOrg.org_name}
+          isOpen={Boolean(configOrg)}
+          onClose={() => onDeletePopup(null)}
+          onCancelClick={() => onDeletePopup(null)}
+          onDeleteClick={() => {}}
+        />
       )}
-    </div>
-  </SectionDropdown>
-);
+    </>
+  );
+};
 
 export default OrganizationsList;
