d299fb9cd78e1039da25e9c6e87a198cb0fdc762
what1s1ove <zubko.2007@gmail.com>
2020-03-05 13:04:13 +0200

    TM-140-Validation: + validation on Division and pool

---
+++ src/components/divisions-and-pools/logic/validation-schemas.ts
@@ -0,0 +1,12 @@
+import * as Yup from 'yup';
+
+const divisionSchema = Yup.object({
+  long_name: Yup.string().required('Division long name is required to fill!'),
+  short_name: Yup.string().required('Division short name is required to fill!'),
+});
+
+const poolSchema = Yup.object({
+  pool_name: Yup.string().required('Pool name is required to fill!'),
+});
+
+export { divisionSchema, poolSchema };
