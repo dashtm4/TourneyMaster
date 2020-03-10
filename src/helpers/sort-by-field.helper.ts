99c44794564d8b344a118fc9641cbbbf47b8bdc0
what1s1ove <zubko.2007@gmail.com>
2020-03-10 17:54:54 +0200

    TM-153-FacAbbr: + simple sort with numbers

--- src/helpers/sort-by-field.helper.ts
+++ src/helpers/sort-by-field.helper.ts
@@ -1,5 +1,7 @@
 function sortByField<T>(arr: T[], field: string): string[] {
-  return arr.map(it => it[field]).sort((a, b) => (a > b ? 1 : -1));
+  return arr
+    .map(it => it[field])
+    .sort((a, b) => a.localeCompare(b, undefined, { numeric: 'true' }));
 }
 
 export { sortByField };
