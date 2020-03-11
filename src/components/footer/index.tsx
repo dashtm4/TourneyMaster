8f88760f2117449b9183937082b485b125f87010
r-khanas <khanasrostyslav@gmail.com>
2020-03-11 14:49:55 +0200

    task/TM-144-Footer, add styling

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from './styles.module.scss';
 import logo from 'assets/logo.png';
 import history from '../../browserhistory';
+import PlaceIcon from '@material-ui/icons/Place';
+import EmailIcon from '@material-ui/icons/Email';
+import PhoneIcon from '@material-ui/icons/Phone';
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = () => {
   const onLogoClick = () => {
@@ -9,15 +13,61 @@ const Footer: React.FC = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <img
-        src={logo}
-        onClick={onLogoClick}
-        className={styles.logo}
-        alt="logo"
-      />
-      <div>&copy; 2020 Tourney Master</div>
-    </div>
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerLeft}>
+        <img
+          src={logo}
+          onClick={onLogoClick}
+          className={styles.logo}
+          alt="logo"
+        />
+
+        <p className={styles.footerCopyright}>Tourney Master &copy; 2020</p>
+      </div>
+
+      <div className={styles.footerCenter}>
+        <div className={styles.footerWithIcon}>
+          <PlaceIcon />
+          <p>1111 Park Ave New York, NY</p>
+        </div>
+
+        <div className={styles.footerWithIcon}>
+          <PhoneIcon />
+          <p>+1.555.555.5555</p>
+        </div>
+
+        <div className={styles.footerWithIcon}>
+          <EmailIcon />
+          <p>email@email.com</p>
+        </div>
+      </div>
+
+      <div className={styles.footerRight}>
+        <ul className={styles.footerLinks}>
+          <li>
+            <Link to="/dashboard">My Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/library-manager">Library Manager</Link>
+          </li>
+          <li>
+            <Link to="/event-link">EventLink</Link>
+          </li>
+          <li>
+            <Link to="/collaboration">Collaboration</Link>
+          </li>
+          <li>
+            <Link to="/calendar">Calendar</Link>
+          </li>
+          <li>
+            <Link to="/utilities">Utilities</Link>
+          </li>
+          <li>
+            <Link to="event-day-complexities">Event Day Complexities</Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 };
 
