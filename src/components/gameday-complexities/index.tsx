5bc76a606bbeef6ab7e5fdd94a34910cbfa95aaf
r-khanas <rostyslav.khanas@binary-studio.com>
2020-03-16 11:07:07 +0200

    task/TM-34-EventDayComplexities, add layout for backup plan and expand/collapse all functionality

--- src/components/gameday-complexities/index.tsx
+++ src/components/gameday-complexities/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Paper, CardMessage } from 'components/common';
+import { Button, Paper, CardMessage, HeadingLevelTwo } from 'components/common';
 import styles from './styles.module.scss';
 import rain from '../../assets/rain.svg';
 import Modal from 'components/common/modal';
@@ -9,6 +9,7 @@ import { connect } from 'react-redux';
 import { getEvents, getFacilities, getFields } from './logic/actions';
 import { BindingAction, IFacility } from 'common/models';
 import { IField } from 'common/models';
+import BackupPlan from './backup-plan';
 
 interface Props {
   getEvents: BindingAction;
@@ -19,14 +20,54 @@ interface Props {
   fields: IField[];
 }
 
-class GamedayComplexities extends React.Component<Props> {
-  state = { isModalOpen: false };
+interface State {
+  isModalOpen: boolean;
+  expanded: boolean[];
+  expandAll: boolean;
+}
+
+const mockData = [
+  {
+    name: "The Predicted Nor'easter Backup Plan",
+    eventName: "Men's Spring Thaw",
+    type: 'Cancel Games',
+  },
+  {
+    name: "The Predicted Nor'easter Backup Plan",
+    eventName: "Men's Spring Thaw",
+    type: 'Cancel Games',
+  },
+];
+
+class GamedayComplexities extends React.Component<Props, State> {
+  state = { isModalOpen: false, expanded: [], expandAll: false };
 
   componentDidMount() {
     this.props.getEvents();
     this.props.getFacilities();
     this.props.getFields();
   }
+
+  componentDidUpdate(_prevProps: any, prevState: any) {
+    if (!prevState.expanded.length) {
+      this.setState({ expanded: mockData.map(_plan => true) });
+    }
+  }
+
+  onToggleAll = () => {
+    this.setState({
+      expanded: this.state.expanded.map(_e => this.state.expandAll),
+      expandAll: !this.state.expandAll,
+    });
+  };
+
+  onToggleOne = (indexPanel: number) => {
+    this.setState({
+      expanded: this.state.expanded.map((e: boolean, index: number) =>
+        index === indexPanel ? !e : e
+      ),
+    });
+  };
 
   onCreatePlan = () => {
     this.setState({ isModalOpen: true });
@@ -35,6 +76,24 @@ class GamedayComplexities extends React.Component<Props> {
   onModalClose = () => {
     this.setState({ isModalOpen: false });
   };
+
+  renderEmpty = () => {
+    return (
+      <div className={styles.sectionContainer}>
+        <p className={styles.infoMessage}>
+          <span>Shit happens.</span> Create contingency plans here so that your
+          event runs smoothly when it needs to
+        </p>
+        <img src={rain} className={styles.image} alt="rain" />
+        <div className={styles.cardMessageContainer}>
+          <CardMessage type="emodjiObjects">
+            Click “Create Backup Plan” in the Utility Bar to get started
+          </CardMessage>
+        </div>
+      </div>
+    );
+  };
+
   render() {
     return (
       <>
@@ -49,26 +108,40 @@ class GamedayComplexities extends React.Component<Props> {
             />
           </div>
         </Paper>
-        <div className={styles.sectionContainer}>
-          <p className={styles.infoMessage}>
-            <span>Shit happens.</span> Create contingency plans here so that
-            your event runs smoothly when it needs to
-          </p>
-          <img src={rain} className={styles.image} alt="rain" />
-          <div className={styles.cardMessageContainer}>
-            <CardMessage type="emodjiObjects">
-              Click “Create Backup Plan” in the Utility Bar to get started
-            </CardMessage>
-          </div>
-          <Modal isOpen={this.state.isModalOpen} onClose={this.onModalClose}>
-            <CreateBackupModal
-              onCancel={this.onModalClose}
-              events={this.props.events}
-              facilities={this.props.facilities}
-              fields={this.props.fields}
-            />
-          </Modal>
+        <div className={styles.headingContainer}>
+          <HeadingLevelTwo>Event Day Complexities</HeadingLevelTwo>
         </div>
+        {mockData.length && (
+          <div className={styles.buttonContainer}>
+            <Button
+              label={this.state.expandAll ? 'Expand All' : 'Collapse All'}
+              variant="text"
+              color="secondary"
+              onClick={this.onToggleAll}
+            />
+          </div>
+        )}
+        {mockData.length
+          ? mockData.map((plan, index) => (
+              <BackupPlan
+                key={index}
+                name={plan.name}
+                eventName={plan.eventName}
+                type={plan.type}
+                expanded={this.state.expanded[index]}
+                index={index}
+                onToggleOne={this.onToggleOne}
+              />
+            ))
+          : this.renderEmpty()}
+        <Modal isOpen={this.state.isModalOpen} onClose={this.onModalClose}>
+          <CreateBackupModal
+            onCancel={this.onModalClose}
+            events={this.props.events}
+            facilities={this.props.facilities}
+            fields={this.props.fields}
+          />
+        </Modal>
       </>
     );
   }
