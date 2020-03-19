bb7756a83869869b598b8b2d817b8c74789d4745
what1s1ove <zubko.2007@gmail.com>
2020-03-19 11:42:44 +0200

    TM-159-Reporting: + base-markup

--- src/pdg-layouts/schedule-table.tsx
+++ src/pdg-layouts/schedule-table.tsx
@@ -4,47 +4,96 @@ import {
   Text,
   View,
   Document,
+  Image,
   StyleSheet,
   PDFDownloadLink,
   PDFViewer,
 } from '@react-pdf/renderer';
+import Api from 'api/api';
+import { Loader } from 'components/common';
+import { IEventDetails } from 'common/models';
+import logo from 'assets/tournamentLogoExample.svg';
 
 const styles = StyleSheet.create({
   page: {
+    padding: 25,
+  },
+  header: {
     flexDirection: 'row',
-    backgroundColor: '#E4E4E4',
+    justifyContent: 'space-between',
   },
   section: {
     margin: 10,
     padding: 10,
-    flexGrow: 1,
   },
 });
 
-const MyDocument = () => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
-    </Page>
-  </Document>
-);
+interface IPDFProps {
+  event: IEventDetails;
+}
 
-export default () => (
-  <>
-    <p>
-      <PDFViewer>
-        <MyDocument />
-      </PDFViewer>
-    </p>
-    <p>
-      <PDFDownloadLink document={<MyDocument />} fileName="somename.pdf">
-        Download
-      </PDFDownloadLink>
-    </p>
-  </>
-);
+const MyDocument = ({ event }: IPDFProps) => {
+  return (
+    <Document>
+      <Page size="A4" orientation="landscape" style={styles.page}>
+        <View style={styles.header}>
+          <View>
+            <Text>{event.event_name}</Text>
+            <Text>Event Schedule ({'<< Schedule Name>>'})</Text>
+          </View>
+          <View>
+            <Image src={logo} />
+            <Text>Img</Text>
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+};
+
+interface IPageState {
+  event: IEventDetails | null;
+}
+
+class PDF extends React.Component<any, IPageState> {
+  constructor(props: null) {
+    super(props);
+
+    this.state = {
+      event: null,
+    };
+  }
+
+  async componentDidMount() {
+    const event = await Api.get('/events?event_id=ADLNT001');
+
+    this.setState({ event: event[0] });
+  }
+
+  render() {
+    const { event } = this.state;
+
+    if (!event) {
+      return <Loader />;
+    }
+
+    const WrappedPDF = () => <MyDocument event={event} />;
+
+    return (
+      <>
+        <p>
+          <PDFViewer width="500" height="400">
+            <WrappedPDF />
+          </PDFViewer>
+        </p>
+        <p>
+          <PDFDownloadLink document={<WrappedPDF />} fileName="somename.pdf">
+            {event.event_name}
+          </PDFDownloadLink>
+        </p>
+      </>
+    );
+  }
+}
+
+export default PDF;
