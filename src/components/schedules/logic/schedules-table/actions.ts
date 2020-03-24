1b31f2986d139ea2f7c0fb6891c3f142a80e142f
Yevhen Prybytok <yevhen.prybytok@binary-studio.com>
2020-03-24 14:48:03 +0200

    TM-64: Added undo and removed typos

--- src/components/schedules/logic/schedules-table/actions.ts
+++ src/components/schedules/logic/schedules-table/actions.ts
@@ -1,14 +1,22 @@
 import { ITeamCard } from 'common/models/schedule/teams';
 import { Dispatch } from 'redux';
-import { SCHEDULES_TABLE_FILL } from './actionTypes';
+import { SCHEDULES_TABLE_FILL, SCHEDULES_TABLE_UNDO } from './actionTypes';
 
 const fillSchedulesTableAction = (payload: ITeamCard[]) => ({
   type: SCHEDULES_TABLE_FILL,
   payload,
 });
 
+const scheduleUndo = () => ({
+  type: SCHEDULES_TABLE_UNDO,
+});
+
 export const fillSchedulesTable = (teamCards: ITeamCard[]) => (
   dispatch: Dispatch
 ) => {
   dispatch(fillSchedulesTableAction(teamCards));
+};
+
+export const onScheduleUndo = () => (dispatch: Dispatch) => {
+  dispatch(scheduleUndo());
 };
