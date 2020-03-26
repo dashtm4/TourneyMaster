ff4ca9e7a0d9499a714be34b0e98dc0cdba009ee
what1s1ove <zubko.2007@gmail.com>
2020-03-26 15:51:39 +0200

    ArchitectDefaultVal: * sort by time  helper

--- src/components/scheduling/tournament-play/index.tsx
+++ src/components/scheduling/tournament-play/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import TournamentPlayItem from '../tournament-play-item';
 import { SectionDropdown, HeadingLevelThree } from 'components/common';
-import { sortByTime } from 'helpers';
+import { compareTime } from 'helpers';
 import { BindingCbWithOne } from 'common/models';
 import { EventMenuTitles } from 'common/enums';
 import { ISchedulingSchedule } from '../types';
@@ -15,7 +15,12 @@ interface IProps {
 
 export default (props: IProps) => {
   const { schedules, eventId, onEditSchedule } = props;
-  const sortedScheduleByName = sortByTime(schedules);
+
+  const sortedScheduleByName = schedules.sort(
+    (a, b) =>
+      compareTime(a.updated_datetime, b.updated_datetime) ||
+      compareTime(a.created_datetime, b.created_datetime)
+  );
 
   return (
     <SectionDropdown
--- src/helpers/sort-by.helper.ts
+++ src/helpers/sort-by.helper.ts
@@ -1,4 +1,3 @@
-import { compareTime } from 'helpers';
 import { SortByFilesTypes } from 'common/enums';
 
 const sortByField = <T>(arr: T[], field: SortByFilesTypes): T[] => {
@@ -13,30 +12,4 @@ const sortTitleByField = <T>(arr: T[], field: SortByFilesTypes): string[] => {
     .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
 };
 
-const sortByTime = <T>(arr: T[]): T[] => {
-  const updtedByArr = arr
-    .filter(it => it[SortByFilesTypes.UPDATED_DATETIME])
-    .sort((a, b) =>
-      compareTime(
-        a[SortByFilesTypes.UPDATED_DATETIME],
-        b[SortByFilesTypes.UPDATED_DATETIME]
-      )
-    );
-
-  const createdByArr = arr
-    .filter(
-      it =>
-        it[SortByFilesTypes.CREATED_DATETIME] &&
-        !it[SortByFilesTypes.UPDATED_DATETIME]
-    )
-    .sort((a, b) =>
-      compareTime(
-        a[SortByFilesTypes.CREATED_DATETIME],
-        b[SortByFilesTypes.CREATED_DATETIME]
-      )
-    );
-
-  return [...updtedByArr, ...createdByArr];
-};
-
-export { sortByField, sortTitleByField, sortByTime };
+export { sortByField, sortTitleByField };
