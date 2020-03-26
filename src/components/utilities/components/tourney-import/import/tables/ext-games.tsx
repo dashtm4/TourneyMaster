40a719cc4dd842dd356e205c73685bcb3537ce57
Elias Diek <elias@holamicasa.com>
2020-03-27 01:39:10 +0800

    update table headers and sortable table in tourney import wizard

--- src/components/utilities/components/tourney-import/import/tables/ext-games.tsx
+++ src/components/utilities/components/tourney-import/import/tables/ext-games.tsx
@@ -1,185 +1,302 @@
 import React from 'react';
-import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableFooter from '@material-ui/core/TableFooter';
 import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
 
-const useStyles1 = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexShrink: 0,
-      marginLeft: theme.spacing(2.5),
-    },
-  }),
-);
+interface Data {
+  row_num: string | number;
+  job_id: string | number;
+  IDTournament: string | number;
+  IDGame: string | number;
+  tournament_endpoint: string | number;
+  tournament_name: string | number;
+  tournament_division_id: string | number;
+  tournament_division_name: string | number;
+  time_period: string | number;
+  location_id: string | number;
+  IDComplex: string | number;
+  location_name: string | number;
+  last_update: string | number;
+  game_date: string | number;
+  game_id: string | number;
+  game_time: string | number;
+  away_team_id: string | number;
+  away_team_name: string | number;
+  home_team_id: string | number;
+  home_team_name: string | number;
+  away_score: string | number;
+  home_score: string | number;
+  is_active_YN: string | number;
+  created_by: string | number;
+  created_datetime: string | number;
+  updated_by: string | number;
+  updated_datetime: string | number;
+}
 
 interface Props {
   games: any[];
 };
 
-interface TablePaginationActionsProps {
-  count: number;
-  page: number;
-  rowsPerPage: number;
-  onChangePage: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
 }
 
-function TablePaginationActions(props: TablePaginationActionsProps) {
-  const classes = useStyles1();
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onChangePage } = props;
+type Order = 'asc' | 'desc';
 
-  const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    onChangePage(event, 0);
-  };
+function getComparator<Key extends keyof any>(
+  order: Order,
+  orderBy: Key,
+): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
 
-  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    onChangePage(event, page - 1);
-  };
+function stableSort<T>(array: T[], comparator: (a: T, b: T) => number) {
+  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map(el => el[0]);
+}
 
-  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    onChangePage(event, page + 1);
-  };
+interface HeadCell {
+  id: keyof Data;
+  label: string;
+}
 
-  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+const headCells: HeadCell[] = [
+  { id: 'tournament_name', label: 'Tournament Name' },
+  { id: 'IDGame', label: 'ID Game' },
+  { id: 'tournament_division_name', label: 'Division Name' },
+  { id: 'time_period', label: 'Date' },
+  { id: 'location_id', label: 'Location' },
+  { id: 'location_name', label: 'Field' },
+  { id: 'game_date', label: 'Game Date' },
+  { id: 'game_time', label: 'Game Time' },
+  { id: 'away_team_name', label: 'Away Team Name' },
+  { id: 'home_team_name', label: 'Home Team Name' },
+  { id: 'away_score', label: 'Away Score' },
+  { id: 'home_score', label: 'Home Score' }
+];
+
+interface EnhancedTableProps {
+  classes: ReturnType<typeof useStyles>;
+  numSelected: number;
+  onRequestSort: (event: React.MouseEvent<unknown>, property: keyof Data) => void;
+  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => void;
+  order: Order;
+  orderBy: string;
+  rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+  const { classes, order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property: keyof Data) => (event: React.MouseEvent<unknown>) => {
+    onRequestSort(event, property);
   };
 
   return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
+    <TableHead>
+      <TableRow>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align="left"
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <span className={classes.visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </span>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
   );
 }
 
-const useStyles2 = makeStyles({
-  table: {
-    minWidth: 500,
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+    paper: {
+      width: '100%',
+      marginBottom: theme.spacing(2),
+    },
+    table: {
+      minWidth: 750,
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: 'rect(0 0 0 0)',
+      height: 1,
+      margin: -1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      top: 20,
+      width: 1,
+    },
+  }),
+);
 
-const ExtGamesTable = ({ games }: Props) => {
-  const classes = useStyles2();
+const ExtLocationsTable = ({ games }: Props) => {
+  const classes = useStyles();
+  const [order, setOrder] = React.useState<Order>('desc');
+  const [orderBy, setOrderBy] = React.useState<keyof Data>('tournament_division_name');
+  const [selected, setSelected] = React.useState<any[]>([]);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, games.length - page * rowsPerPage);
+  const handleRequestSort = (event: React.MouseEvent<unknown>, property: keyof Data) => {
+    event.preventDefault();
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
 
-  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
-    if (event !== null)
-      event.preventDefault();
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelecteds = games.map((n, index) => {
+        console.log(n);
+        return index;
+      });
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, name: string | number) => {
+    event.preventDefault();
+    const selectedIndex = selected.indexOf(name);
+    let newSelected: any[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    console.log(event);
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
 
-  return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="custom pagination table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="left">Tournament Name</TableCell>
-            <TableCell align="left">ID Game</TableCell>
-            <TableCell align="left">Tournament Division Name</TableCell>
-            <TableCell align="left">Time Period</TableCell>
-            <TableCell align="left">Location ID</TableCell>
-            <TableCell align="left">Location Name</TableCell>
-            <TableCell align="left">Game Date</TableCell>
-            <TableCell align="left">Game Time</TableCell>
-            <TableCell align="left">Away Team Name</TableCell>
-            <TableCell align="left">Home Team Name</TableCell>
-            <TableCell align="left">Away Score</TableCell>
-            <TableCell align="left">Home Score</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(rowsPerPage > 0
-            ? games.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : games
-          ).map((row, index) => (
-            <TableRow key={index}>
-              <TableCell component="th" scope="row">
-                {row.tournament_name}
-              </TableCell>
-              <TableCell align="left">{row.IDGame}</TableCell>
-              <TableCell align="left">{row.tournament_division_name}</TableCell>
-              <TableCell align="left">{row.time_period}</TableCell>
-              <TableCell align="left">{row.location_id}</TableCell>
-              <TableCell align="left">{row.location_name}</TableCell>
-              <TableCell align="left">{row.game_date}</TableCell>
-              <TableCell align="left">{row.game_time}</TableCell>
-              <TableCell align="left">{row.away_team_name}</TableCell>
-              <TableCell align="left">{row.home_team_name}</TableCell>
-              <TableCell align="left">{row.away_score}</TableCell>
-              <TableCell align="left">{row.home_score}</TableCell>
-            </TableRow>
-          ))}
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
-              <TableCell colSpan={6} />
-            </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-              colSpan={12}
-              count={games.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: { 'aria-label': 'rows per page' },
-                native: true,
-              }}
-              onChangePage={handleChangePage}
-              onChangeRowsPerPage={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
-  );
-}
+  const isSelected = (name: string | number) => selected.indexOf(name) !== -1;
 
-export default ExtGamesTable;
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, games.length - page * rowsPerPage);
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <TableContainer>
+          <Table
+            className={classes.table}
+            aria-labelledby="tableTitle"
+            aria-label="enhanced table"
+          >
+            <EnhancedTableHead
+              classes={classes}
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={handleSelectAllClick}
+              onRequestSort={handleRequestSort}
+              rowCount={games.length}
+            />
+            <TableBody>
+              {stableSort(games, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const isItemSelected = isSelected(index);
+                  const labelId = `enhanced-table-checkbox-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => handleClick(event, index)}
+                      tabIndex={-1}
+                      key={index}
+                      selected={isItemSelected}
+                    >
+                      <TableCell align="left">{row.tournament_name}</TableCell>
+                      <TableCell component="th" id={labelId} scope="row" padding="none">
+                        {row.IDGame}
+                      </TableCell>
+                      <TableCell align="left">{row.tournament_division_name}</TableCell>
+                      <TableCell align="left">{row.time_period}</TableCell>
+                      <TableCell align="left">{row.location_id}</TableCell>
+                      <TableCell align="left">{row.location_name}</TableCell>
+                      <TableCell align="left">{row.game_date}</TableCell>
+                      <TableCell align="left">{row.game_time}</TableCell>
+                      <TableCell align="left">{row.away_team_name}</TableCell>
+                      <TableCell align="left">{row.home_team_name}</TableCell>
+                      <TableCell align="left">{row.away_score}</TableCell>
+                      <TableCell align="left">{row.home_score}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={games.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </div>
+  );
+};
+
+export default ExtLocationsTable;
