ec00ff016237cbe980b86fa7d80e7a53994d9772
r-khanas <rostyslav.khanas@binary-studio.com>
2020-03-31 09:30:51 +0300

    task/TM-173-Small-improvements, disable ability to  drop team into locked team position

--- src/components/common/matrix-table/dnd/drop.tsx
+++ src/components/common/matrix-table/dnd/drop.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDrop } from 'react-dnd';
 import styles from './styles.module.scss';
+import { ITeamCard } from 'common/models/schedule/teams';
 
 export interface IDropParams {
   teamId: string;
@@ -15,10 +16,23 @@ interface IProps {
   position: 1 | 2;
   children?: React.ReactElement;
   onDrop: (dropParams: IDropParams) => void;
+  teamCards: ITeamCard[];
 }
 
 const DropContainer = (props: IProps) => {
-  const { acceptType, onDrop, children } = props;
+  const { acceptType, onDrop, children, gameId, teamCards } = props;
+
+  const isTeamLocked = teamCards
+    .map(team => team.games)
+    .flat()
+    .filter(
+      (game: { id: number; position: number; isTeamLocked: boolean }) =>
+        game.id === gameId
+    )
+    .filter(
+      (game: { id: number; teamPosition: number; isTeamLocked: boolean }) =>
+        game.teamPosition === props.position
+    )[0]?.isTeamLocked;
 
   const [{ isOver }, drop] = useDrop({
     accept: acceptType,
@@ -32,15 +46,15 @@ const DropContainer = (props: IProps) => {
     },
     collect: mon => ({
       isOver: !!mon.isOver(),
-      canDrop: !!mon.canDrop(),
     }),
+    canDrop: () => !isTeamLocked,
   });
 
   return (
     <div
       ref={drop}
       className={styles.dropContainer}
-      style={{ opacity: isOver ? 0.5 : 1 }}
+      style={{ opacity: isOver && !isTeamLocked ? 0.5 : 1 }}
     >
       {children}
     </div>
