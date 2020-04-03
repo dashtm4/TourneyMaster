6a0da918c75a3c4242e5e0d223800c220a528bf4
Yevhen Prybytok <yevhen.prybytok@binary-studio.com>
2020-04-03 11:41:31 +0300

    TM-186: Add pools to schedules column

--- src/components/common/table-schedule/index.tsx
+++ src/components/common/table-schedule/index.tsx
@@ -202,6 +202,7 @@ const TableSchedule = ({
         <DndProvider backend={HTML5Backend}>
           {tableType === TableScheduleTypes.SCHEDULES && (
             <ListUnassigned
+              pools={pools}
               tableType={tableType}
               teamCards={teamCards}
               minGamesNum={minGamesNum}
