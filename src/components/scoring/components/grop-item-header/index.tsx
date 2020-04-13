ad54a3b02c8f26a05c8abd7966ecae0ab77326e2
what1s1ove <zubko.2007@gmail.com>
2020-04-13 17:46:47 +0300

    Reporting0412: + tooltips for the scoring  table

--- src/components/scoring/components/grop-item-header/index.tsx
+++ src/components/scoring/components/grop-item-header/index.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
+import { Tooltip } from 'components/common';
+import { TooltipMessageTypes } from 'components/common/tooltip-message/types';
 import { IScoringSetting } from 'common/models';
 
 interface Props {
   scoringSettings: IScoringSetting;
 }
 
+const getHeadingWithTooltip = (heading: string, message: string) => (
+  <th>
+    <Tooltip type={TooltipMessageTypes.INFO} title={message}>
+      <span>{heading}</span>
+    </Tooltip>
+  </th>
+);
+
 const GroupItemHeader = ({ scoringSettings }: Props) => (
   <thead>
     <tr>
       <th>Team</th>
-      <th>W</th>
-      <th>L</th>
-      {scoringSettings.hasTies && <th>T</th>}
-      {scoringSettings.hasGoalsScored && <th>GS</th>}
-      {scoringSettings.hasGoalsAllowed && <th>GA</th>}
-      {scoringSettings.hasGoalsDifferential && <th>GD</th>}
+      {getHeadingWithTooltip('W', 'Wins')}
+      {getHeadingWithTooltip('L', 'Losses')}
+      {scoringSettings.hasTies && getHeadingWithTooltip('T', 'Ties')}
+      {scoringSettings.hasGoalsScored &&
+        getHeadingWithTooltip('GS', 'Goals Scored')}
+      {scoringSettings.hasGoalsAllowed &&
+        getHeadingWithTooltip('GA', 'Goals Allowed')}
+      {scoringSettings.hasGoalsDifferential &&
+        getHeadingWithTooltip('GD', 'Goals Differential')}
     </tr>
   </thead>
 );
