521b84ae656f66a628d39dc9899251784e01388f
r-khanas <rostyslav.khanas@binary-studio.com>
2020-04-13 13:31:46 +0300

    fix/csv-uploader, fix error message when no division provided

--- src/components/divisions-and-pools/add-division/index.tsx
+++ src/components/divisions-and-pools/add-division/index.tsx
@@ -195,7 +195,7 @@ class AddDivision extends React.Component<IDivisionProps, IAddDivisionState> {
   }
 
   render() {
-    const { short_name }: Partial<IDivision> = this.state.divisions[0] || '';
+    const { long_name }: Partial<IDivision> = this.state.divisions[0] || '';
     const deleteMessage = `You are about to delete this division and this cannot be undone.
     Deleting a division will also delete all pools (${this.props.location.state?.pools.length}) and teams (${this.props.location.state?.teams.length}) inside the division.
     Please, enter the name of the division to continue.`;
@@ -235,7 +235,7 @@ class AddDivision extends React.Component<IDivisionProps, IAddDivisionState> {
         <DeletePopupConfrim
           type={'division'}
           message={deleteMessage}
-          deleteTitle={short_name || ''}
+          deleteTitle={long_name || ''}
           isOpen={this.state.isModalOpen}
           onClose={this.onModalClose}
           onDeleteClick={this.onDeleteDivision}
