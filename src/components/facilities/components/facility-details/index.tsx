72c57250c672c376928c29b911feb940cc91123d
Daniel Cashion <danielcashion@users.noreply.github.com>
2020-04-17 15:55:30 -0400

    corrected "golf_carts_able"

--- src/components/facilities/components/facility-details/index.tsx
+++ src/components/facilities/components/facility-details/index.tsx
@@ -43,7 +43,7 @@ enum FormFields {
   LAST_GAME_END = 'last_game_end',
   PARKING_AVAILABLE = 'parking_available',
   PARKING_PROXIMITY = 'parking_proximity',
-  GOLF_CARTS_AVAILABE = 'golf_carts_availabe',
+  GOLF_CARTS_AVAILABLE = 'golf_carts_available',
   PARKING_DETAILS = 'parking_details',
 }
 
@@ -434,7 +434,7 @@ class FacilityDetails extends React.Component<Props, State> {
                 options={[
                   {
                     label: 'Golf Carts Available',
-                    checked: Boolean(facility.golf_carts_availabe),
+                    checked: Boolean(facility.golf_carts_available),
                     disabled: !isEdit,
                   },
                 ]}
