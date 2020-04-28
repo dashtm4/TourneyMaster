ef8c168fb155cf3f97109063e5d8e2165260e1d5
Yevhen Prybytok <yevhen.prybytok@binary-studio.com>
2020-04-28 11:55:03 +0300

    TM-255: Dnd warnings for Brackets

--- src/components/playoffs/helper.ts
+++ src/components/playoffs/helper.ts
@@ -1,9 +1,14 @@
-import { orderBy } from 'lodash-es';
+import { orderBy, union, min } from 'lodash-es';
 import { IGame } from 'components/common/matrix-table/helper';
 import { IBracketGame } from './bracketGames';
 import { IOnAddGame } from './add-game-modal';
 import { getVarcharEight } from 'helpers';
 import { IDivision, IField } from 'common/models';
+
+interface IBracketMoveWarning {
+  gameAlreadyAssigned: boolean;
+  gamePlayTimeInvalid: boolean;
+}
 
 export const updateGameBracketInfo = (game: IGame, withGame?: IGame) => ({
   ...game,
@@ -117,7 +122,7 @@ const getDependentGames = (
   const newGames = games.filter(item => !foundGamesInds.includes(item.index));
 
   if (!foundGamesInds?.length || !newGames?.length) {
-    return newDependentInds;
+    return union(newDependentInds);
   }
 
   return getDependentGames(newDependentInds, newGames);
@@ -177,6 +182,33 @@ export const updateGameSlot = (
   playoffIndex: bracketGame?.index,
 });
 
+enum BracketMoveWarnEnum {
+  gameAlreadyAssigned = 'This bracket game is already assigned. Are you sure you want to continue?',
+  gamePlayTimeInvalid = 'This bracket game cannot be placed at this time.',
+}
+
+export const setReplacementMessage = (
+  bracketGames: IBracketGame[],
+  warnings: IBracketMoveWarning
+): {
+  bracketGames?: IBracketGame[];
+  message?: string;
+} | null => {
+  switch (true) {
+    case warnings.gamePlayTimeInvalid:
+      return {
+        message: BracketMoveWarnEnum.gamePlayTimeInvalid,
+      };
+    case warnings.gameAlreadyAssigned:
+      return {
+        bracketGames,
+        message: BracketMoveWarnEnum.gameAlreadyAssigned,
+      };
+    default:
+      return null;
+  }
+};
+
 export const updateBracketGame = (
   bracketGame: IBracketGame,
   gameSlot?: IGame,
@@ -196,15 +228,16 @@ export const updateBracketGamesDndResult = (
   games: IGame[],
   fields: IField[]
 ) => {
+  const warnings: IBracketMoveWarning = {
+    gameAlreadyAssigned: false,
+    gamePlayTimeInvalid: false,
+  };
   // 1. Find a bracket game that is being dragged
   const bracketGame = bracketGames.find(item => item.id === gameId);
-
   // 2. Find a game slot where the bracket game is gonna be placed
   const gameSlot = games.find(item => item.id === slotId);
-
   // 3. Check if the bracket game is not placed anywhere else
   //  3.1. If so - return a warning popup
-
   // 4. Populate the bracket data with the game slot data
   const fieldName = fields.find(item => item.field_id === gameSlot?.fieldId)
     ?.field_name;
@@ -215,5 +248,39 @@ export const updateBracketGamesDndResult = (
       : item
   );
 
-  return { bracketGames };
+  // Warnings setup
+  // Find if a game is already assigned
+  const bracketGameToUpdate = bracketGames.find(
+    item => item.id === bracketGame?.id
+  );
+  warnings.gameAlreadyAssigned = Boolean(
+    bracketGame?.fieldId &&
+      bracketGame?.startTime &&
+      bracketGameToUpdate?.fieldId &&
+      bracketGameToUpdate.startTime
+  );
+  // Find if a game play time is invalid
+  const divisionGames = [...bracketGames].filter(
+    item => item.divisionId === bracketGame?.divisionId
+  );
+  const dependentInds = getDependentGames(
+    [bracketGame?.index || -1],
+    divisionGames
+  );
+  const dependentStartTimes = divisionGames
+    .filter(item => item.index !== bracketGame?.index)
+    .filter(item => dependentInds.includes(item.index))
+    .map(item => item.startTime);
+
+  const minStartTimeAvailable = min(dependentStartTimes);
+  const bracketNewTime = divisionGames.find(
+    item => item.index === bracketGame?.index
+  )?.startTime;
+
+  warnings.gamePlayTimeInvalid =
+    !!bracketNewTime &&
+    !!minStartTimeAvailable &&
+    bracketNewTime >= minStartTimeAvailable;
+
+  return { bracketGames, warnings };
 };
