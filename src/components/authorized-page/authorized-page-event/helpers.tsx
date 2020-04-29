d95bbaaa62b6ec9e655eb43cbcb4ec065165162e
what1s1ove <zubko.2007@gmail.com>
2020-04-29 13:02:42 +0300

    TM-275-PublishEvent: + check publish helper

--- src/components/authorized-page/authorized-page-event/helpers.tsx
+++ src/components/authorized-page/authorized-page-event/helpers.tsx
@@ -19,6 +19,48 @@ import {
 import { IInputEvent } from 'common/types';
 import { PublishSettingFields } from './common';
 
+const checkPublishedEvent = (event: IEventDetails): boolean => {
+  const isPublished = event.is_published_YN === EventStatuses.Draft;
+
+  return isPublished;
+};
+
+const checkPublishedSchedule = (schedules: ISchedule[]): boolean => {
+  const hasPublished =
+    schedules.length > 0 &&
+    schedules.every(it => it.schedule_status === ScheduleStatuses.DRAFT);
+
+  return hasPublished;
+};
+
+const checkPublishedBracket = (brackets: IFetchedBracket[]): boolean => {
+  const hasPublished =
+    brackets.length > 0 &&
+    brackets.every(it => it.is_published_YN === BracketStatuses.Draft);
+
+  return hasPublished;
+};
+
+const checkAllPublishes = (
+  event: IEventDetails,
+  schedules: ISchedule[],
+  brackets: IFetchedBracket[]
+): boolean => {
+  const hasAllPublished =
+    checkPublishedEvent(event) &&
+    checkPublishedSchedule(schedules) &&
+    checkPublishedBracket(brackets);
+
+  return hasAllPublished;
+};
+
+const CheckEventPublishes = {
+  checkPublishedEvent,
+  checkPublishedSchedule,
+  checkPublishedBracket,
+  checkAllPublishes,
+};
+
 const getEventPublishOptions = (
   event: IEventDetails,
   schedules: ISchedule[],
@@ -26,21 +68,15 @@ const getEventPublishOptions = (
 ) => {
   const eventPublishOptions = [];
 
-  if (event.is_published_YN === EventStatuses.Draft) {
+  if (CheckEventPublishes.checkPublishedEvent(event)) {
     eventPublishOptions.push(EventPublishTypes.DETAILS);
   }
 
-  if (
-    schedules.length > 0 &&
-    schedules.every(it => it.schedule_status === ScheduleStatuses.DRAFT)
-  ) {
+  if (CheckEventPublishes.checkPublishedSchedule(schedules)) {
     eventPublishOptions.push(EventPublishTypes.DETAILS_AND_TOURNAMENT_PLAY);
   }
 
-  if (
-    brackets.length > 0 &&
-    brackets.every(it => it.is_published_YN === BracketStatuses.Draft)
-  ) {
+  if (CheckEventPublishes.checkPublishedBracket(brackets)) {
     eventPublishOptions.push(
       EventPublishTypes.DETAILS_AND_TOURNAMENT_PLAY_AND_BRACKETS
     );
