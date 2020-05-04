b76b31a14dd986f30a97b75a06a74e2c6fba57a4
what1s1ove <zubko.2007@gmail.com>
2020-05-04 11:37:56 +0300

    PoolDropDown: + can prop & active drop functionality

--- src/components/divisions-and-pools/division/pools-details/pool/index.tsx
+++ src/components/divisions-and-pools/division/pools-details/pool/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useDrop } from 'react-dnd';
-import styles from '../styles.module.scss';
 import { IPool, ITeam, IDivision } from 'common/models';
 import { DndItems } from '../../../common';
 import ItemTeam from '../../item-team';
 import { sortByField } from 'helpers';
 import { SortByFilesTypes } from 'common/enums';
+import { getBackgroundColor } from '../helpers';
+import styles from '../styles.module.scss';
 
 interface IPoolProps {
   division: IDivision;
@@ -32,7 +33,7 @@ const Pool = ({
   onEditPopupOpen,
   toggleChangesAreMade,
 }: IPoolProps) => {
-  const [, drop] = useDrop({
+  const [{ isOver, canDrop }, drop] = useDrop({
     accept: DndItems.TEAM,
     drop: () => {
       toggleChangesAreMade(true);
@@ -41,10 +42,17 @@ const Pool = ({
         poolId: pool ? pool.pool_id : null,
       };
     },
+    collect: monitor => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
   });
 
+  const isActive = isOver && canDrop;
+  const backgroundColor = getBackgroundColor(isActive, canDrop);
+
   return (
-    <div className={styles.pool}>
+    <div className={styles.pool} style={{ backgroundColor }}>
       <p className={styles.poolTitle}>{pool ? pool.pool_name : 'Unassigned'}</p>
       <ul className={styles.teamList} ref={isArrange ? drop : null}>
         {sortByField(teams, SortByFilesTypes.TEAMS).map(it => (
