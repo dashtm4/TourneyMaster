e46527ea4e0c43d08c8c4a5d46b300b1fc1f853b
Roman Zhukov <ruzhukov@gmail.com>
2020-05-08 16:20:43 -0400

    Check Stripe signature in webhook

--- server/functions/uploadLambdaCode.sh
+++ server/functions/uploadLambdaCode.sh
@@ -24,4 +24,4 @@ aws lambda update-function-configuration --function-name "TourneyMasterPayments-
 
 aws lambda update-function-code --function-name "TourneyMasterPayments-PaymentsApiFunction" --zip-file "fileb://dist/bundle.zip"
 aws lambda update-function-configuration --function-name "TourneyMasterPayments-PaymentsApiFunction" \
-  --environment "Variables={STRIPE_PUBLISHABLE_KEY=$STRIPE_PUBLISHABLE_KEY,STRIPE_SECRET_KEY=$STRIPE_SECRET_KEY}"
+  --environment "Variables={STRIPE_PUBLISHABLE_KEY=$STRIPE_PUBLISHABLE_KEY,STRIPE_SECRET_KEY=$STRIPE_SECRET_KEY,STRIPE_WEBHOOK_SIGNING_SECRET=$STRIPE_WEBHOOK_SIGNING_SECRET}"
