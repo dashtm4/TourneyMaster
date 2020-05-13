4f302ffc4554e93499e2b941c745974252404ca0
what1s1ove <zubko.2007@gmail.com>
2020-05-13 14:12:37 +0300

    CancelGames: - remove unused radio options

--- src/components/gameday-complexities/common.ts
+++ src/components/gameday-complexities/common.ts
@@ -2,12 +2,12 @@ import ITimeSlot from 'common/models/schedule/timeSlots';
 
 export enum OptionsEnum {
   'Cancel Games' = 'cancel_games',
-  'Weather Interruption: Modify Game Timeslots' = 'weather_interruption_modify_game_timeslots',
+  // 'Weather Interruption: Modify Game Timeslots' = 'weather_interruption_modify_game_timeslots',
 }
 
 export enum TypeOptionsEnum {
   'cancel_games' = 'Cancel Games',
-  'weather_interruption_modify_game_timeslots' = 'Weather Interruption: Modify Game Timeslots',
+  // 'weather_interruption_modify_game_timeslots' = 'Weather Interruption: Modify Game Timeslots',
 }
 export interface IComplexityTimeslot {
   eventId: string;
