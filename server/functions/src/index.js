e528ea9a13b8703d356fe2c145923eaea8b84bff
Roman Zhukov <ruzhukov@gmail.com>
2020-05-13 09:11:10 -0400

    Stripe signature

--- server/functions/src/index.js
+++ server/functions/src/index.js
@@ -8,30 +8,36 @@ const app = express();
 const router = express.Router();
 
 app.use(cors());
-app.use(express.json());
+app.use(
+  express.json({
+    verify: (req, res, buf, encoding) => {
+      req.rawBody = buf;
+    },
+  })
+);
 app.use((req, res, next) => {
-	console.log(
-		`${req.method} ${req.originalUrl}\nBody: ${JSON.stringify(
-			req.body,
-			null,
-			'  '
-		)}`
-	);
-	next();
+  console.log(
+    `${req.method} ${req.originalUrl}\nBody: ${JSON.stringify(
+      req.body,
+      null,
+      '  '
+    )}`
+  );
+  next();
 });
 app.use('/public/payments', api(router));
 
 const isInLambda = !!process.env.LAMBDA_TASK_ROOT;
 let handler = null;
 if (isInLambda) {
-	const server = serverlessExpress.createServer(app);
-	handler = (event, context) => {
-		console.log('Event: ', JSON.stringify(event));
-		console.log('Context: ', JSON.stringify(context));
-		serverlessExpress.proxy(server, event, context);
-	};
+  const server = serverlessExpress.createServer(app);
+  handler = (event, context) => {
+    console.log('Event: ', JSON.stringify(event));
+    console.log('Context: ', JSON.stringify(context));
+    serverlessExpress.proxy(server, event, context);
+  };
 } else {
-	app.listen(3000, () => console.log(`Listening on 3000`));
+  app.listen(3000, () => console.log(`Listening on 3000`));
 }
 
 export { handler };
