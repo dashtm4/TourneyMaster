a6a08c7f282baaeee2ce08152199da812cabb250
Daniel Cashion <danielcashion@users.noreply.github.com>
2020-06-01 14:06:28 -0400

    Commented  matching logic on phone and went with required.

--- src/validations/team-schema/index.ts
+++ src/validations/team-schema/index.ts
@@ -4,9 +4,11 @@ const teamSchema = Yup.object({
   long_name: Yup.string().required('Team long name is required to fill!'),
 //  short_name: Yup.string().required('Team short name is required to fill!'),
   division_id: Yup.string().required('Division is required to fill!'),
-  phone_num: Yup.string().matches(
-    /^\+?[1-9]\d{1,14}$/,
-    'Invalid format for phone number'
+  phone_num: Yup.string().required('Phone number of coaches are required!'),
+  
+//  phone_num: Yup.string().matches(
+//   /^\+?[1-9]\d{1,14}$/,
+//  'Invalid format for phone number'
   ),
 });
 
