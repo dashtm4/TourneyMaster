9a256ee6cb2ddbc643765e3033eaab8cbc26a562
Daniel Cashion <danielcashion@users.noreply.github.com>
2020-06-01 14:03:32 -0400

    Commented out Short Name

--- src/validations/team-schema/index.ts
+++ src/validations/team-schema/index.ts
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 const teamSchema = Yup.object({
   long_name: Yup.string().required('Team long name is required to fill!'),
-  short_name: Yup.string().required('Team short name is required to fill!'),
+//  short_name: Yup.string().required('Team short name is required to fill!'),
   division_id: Yup.string().required('Division is required to fill!'),
   phone_num: Yup.string().matches(
     /^\+?[1-9]\d{1,14}$/,
