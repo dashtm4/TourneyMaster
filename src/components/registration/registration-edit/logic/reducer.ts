9ea65879044c4adc4201ad78b2004a0294d4c8e4
Roman Zhukov <ruzhukov@gmail.com>
2020-06-02 21:02:20 -0400

    Registrants and payments

--- src/components/registration/registration-edit/logic/reducer.ts
+++ src/components/registration/registration-edit/logic/reducer.ts
@@ -4,15 +4,24 @@ import {
   REGISTRATION_UPDATE_SUCCESS,
   REGISTRATION_FETCH_START,
   DIVISIONS_FETCH_SUCCESS,
+  REGISTRANTS_FETCH_SUCCESS,
+  REGISTRANTS_PAYMENTS_FETCH_SUCCESS,
   EVENT_FETCH_SUCCESS,
 } from './actionTypes';
 import { sortByField } from 'helpers';
-import { IDivision, IRegistration, IEventDetails } from 'common/models';
+import {
+  IDivision,
+  IRegistration,
+  IRegistrant,
+  IEventDetails,
+} from 'common/models';
 import { SortByFilesTypes } from 'common/enums';
 
 export interface IState {
   data?: Partial<IRegistration>;
   divisions: IDivision[];
+  registrants: IRegistrant[];
+  payments: any[];
   event?: IEventDetails;
   isLoading: boolean;
   error: boolean;
@@ -21,6 +30,8 @@ export interface IState {
 const defaultState: IState = {
   data: undefined,
   divisions: [],
+  registrants: [],
+  payments: [],
   event: undefined,
   isLoading: true,
   error: false,
@@ -63,6 +74,21 @@ export default (
         error: false,
       };
     }
+    case REGISTRANTS_FETCH_SUCCESS: {
+      return {
+        ...state,
+        registrants: action.payload,
+        error: false,
+      };
+    }
+    case REGISTRANTS_PAYMENTS_FETCH_SUCCESS: {
+      return {
+        ...state,
+        payments: action.payload,
+        error: false,
+      };
+    }
+
     case EVENT_FETCH_SUCCESS: {
       return { ...state, event: action.payload };
     }
