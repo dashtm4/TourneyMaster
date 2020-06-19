3edb7a4b3c7687baa5bb9bda8c643743781973d2
777777miSSU7777777 <o.fedorovich@yumasoft.com>
2020-06-19 18:01:31 +0300

    added some props & dispatch from global state to VisualGamesMaker component

--- src/components/visual-games-maker/index.tsx
+++ src/components/visual-games-maker/index.tsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import { ITeam } from 'common/models';
-import { IPageEventState } from 'components/authorized-page/authorized-page-event/logic/reducer';
-import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { ITeam, IDivision, IPool } from 'common/models';
+import { IPageEventState } from 'components/authorized-page/authorized-page-event/logic/reducer';
+import { IDivisionAndPoolsState } from 'components/divisions-and-pools/logic/reducer';
+import { getAllPools } from 'components/divisions-and-pools/logic/actions';
 
 interface IMapStateToProps {
   teams?: ITeam[] | undefined;
+  divisions?: IDivision[] | undefined;
+  pools?: IPool[] | undefined;
 }
 
-interface IMapDispatchToProps {}
+interface IMapDispatchToProps {
+  getAllPools: (divisionIds: string[]) => void;
+}
 
 interface IComponentProps {}
 
 interface IRootState {
   pageEvent?: IPageEventState;
+  divisionsAndPools?: IDivisionAndPoolsState;
 }
 
 type IProps = IMapStateToProps & IMapDispatchToProps & IComponentProps;
@@ -21,19 +28,41 @@ type IProps = IMapStateToProps & IMapDispatchToProps & IComponentProps;
 interface IState {}
 
 class VisualGamesMaker extends Component<IProps, IState> {
+  componentDidMount() {
+    const { divisions } = this.props;
+
+    this.props.getAllPools(divisions!.map(v => v.division_id));
+  }
+
+  onDivisionChange(e: any) {
+    const divisionId = e.target.value;
+    this.setState({ selectedDivisionId: divisionId });
+  }
+
   render() {
     return (
-      <>
-        <div>Visual Games Maker</div>
-      </>
+      <div className={styles.container}>
+        Visual Games Maker
+      </div>
     );
   }
 }
 
-const mapStateToProps = ({ pageEvent }: IRootState): IMapStateToProps => ({
+const mapStateToProps = ({
+  pageEvent,
+  divisionsAndPools,
+}: IRootState): IMapStateToProps => ({
   teams: pageEvent?.tournamentData.teams,
+  divisions: pageEvent?.tournamentData.divisions,
+  pools: divisionsAndPools?.pools,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch): IMapDispatchToProps => bindActionCreators({}, dispatch);
+const mapDispatchToProps = (dispatch: Dispatch): IMapDispatchToProps =>
+  bindActionCreators(
+    {
+      getAllPools,
+    },
+    dispatch
+  );
 
 export default connect<IMapStateToProps, IMapDispatchToProps>(mapStateToProps, mapDispatchToProps)(VisualGamesMaker);
