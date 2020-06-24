eefde3caac7a4eab45d4b35df836890bae6364e3
dashtm4 <dashtm4@gmail.com>
2020-06-24 05:59:13 -0400

    feat: implement fab buttons(save and cancel)

---
+++ src/components/common/fab-button/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Button } from 'components/common';
+import styles from './styles.module.scss';
+
+interface IFabButtonState {
+  isBtnVisible: boolean;
+}
+
+interface IFabButtonProps {
+  onClick: () => void;
+  sequence: number | 0;
+  variant: 'text' | 'outlined' | 'contained' | undefined;
+  label: string;
+}
+
+class FabButton extends React.Component<IFabButtonProps, IFabButtonState> {
+  state = { isBtnVisible: false };
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
+  onScroll = () => {
+    if (window.scrollY > 140) {
+      this.setState({ isBtnVisible: true });
+    } else {
+      this.setState({ isBtnVisible: false });
+    }
+  };
+
+  onClick = () => {
+    this.props.onClick();
+  };
+
+  render() {
+    const { label, sequence, variant } = this.props;
+
+    return (
+      <div
+        className={styles.btnContainer}
+        style={{ bottom: `${sequence * 60 + 20}px` }}
+      >
+        <div className={!this.state.isBtnVisible ? styles.hidden : undefined}>
+          <Button
+            label={label}
+            variant={variant}
+            color="primary"
+            onClick={this.onClick}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FabButton;
